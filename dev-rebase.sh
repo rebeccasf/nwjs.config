2a897c93e2259cb826d41a278480e0a75a7333a8
Rebecca Chang Swee Fun <rebecca.chang@starfivetech.com>
2022-11-04 03:25:44 -0400

    bash: archive some scripts to help development
    Signed-off-by: Rebecca Chang Swee Fun <rebecca.chang@starfivetech.com>

---
+++ dev-rebase.sh
@@ -0,0 +1,8 @@
+pushd content/nw && git checkout -- . && popd
+pushd third_party/devtools-frontend/src && git checkout -- . && popd
+pushd third_party/ffmpeg && git checkout -- . && popd
+pushd third_party/icu && git checkout -- . && popd
+pushd third_party/vulkan-deps/vulkan-validation-layers/src && git checkout -- . && popd
+pushd third_party/webdriver/pylib && git checkout -- . && popd
+pushd tools/gyp && git checkout -- . && popd
+pushd v8 && git checkout -- . && popd
